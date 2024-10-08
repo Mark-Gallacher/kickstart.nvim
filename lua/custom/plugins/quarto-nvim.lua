1b1dcee794adc14cb4fab80a0397910d2b91b280
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2024-10-08 18:59:56 +0100

    separating the plugins as individual files

---
+++ lua/custom/plugins/quarto-nvim.lua
@@ -0,0 +1,11 @@
+-- plugins/quarto.lua
+return {
+  {
+    'quarto-dev/quarto-nvim',
+    dependencies = {
+      'jmbuhr/otter.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/keymaps.lua
@@ -0,0 +1,78 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Change the height and width of a window more easier
+vim.keymap.set('n', '<M-,>', '<c-w>5<', { desc = 'Decrease the width of the window' })
+vim.keymap.set('n', '<M-.>', '<c-w>5>', { desc = 'Increase the width of the window' })
+vim.keymap.set('n', '<M-t>', '<c-w>+', { desc = 'Increase the Height of the window -- Taller' })
+vim.keymap.set('n', '<M-s>', '<c-w>-', { desc = 'Decrease the Height of the window -- Shorter' })
+
+--vim.keymap.set('n', '<leader>bn', ':bnext ', { desc = 'Move to Next Buffer' })
+vim.keymap.set('n', '<leader>bn', vim.cmd.bnext, { desc = 'Move to Next Buffer' })
+vim.keymap.set('n', '<leader>bp', vim.cmd.bprevious, { desc = 'Move to Previous Buffer' })
+--vim.keymap.set('n', '<leader>bp', ':bprevious ', { desc = 'Move to Previous /Buffer' })
+
+-- Keymaps to execute the current line or file
+vim.keymap.set('n', '<leader>x', '<cmd>.lua<CR>', { desc = 'Execute the current line' })
+vim.keymap.set('n', '<leader><leader>x', '<cmd>.lua<CR>', { desc = 'Execute the current filel' })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- The general format is to give the autocommand a description, then a group as it is "listening" for our actions,
+-- this isolates the function being called with an action and prevents duplicates callbacks. Then we tell it what to do
+-- after it is finished - ie the callback function.
+--
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+-- vim: ts=2 sts=2 sw=2 et
