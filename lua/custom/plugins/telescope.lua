33c7f23adb0c7fcff642ba3c725bcde4a37b86f8
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2024-10-31 21:27:22 +0000

    adding option to see custom colourschemes only

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  branch = 'master',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -58,7 +58,7 @@ return {
       pickers = {},
       extensions = {
         ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
+          require('telescope.themes').get_dropdown,
         },
       },
     }
@@ -78,7 +78,12 @@ return {
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch (existing) [B]uffers' })
+
+    -- See full list of custom colour schemes - remove default ones
+    vim.keymap.set('n', '<leader>sc', function()
+      builtin.colorscheme { enable_preview = true, ignore_builtins = true }
+    end, { desc = '[S]earch [C]olourschemes' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
