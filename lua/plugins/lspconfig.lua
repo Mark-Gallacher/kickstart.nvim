5550f96b7ec3279b2733899d5b96a51313482484
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-22 18:13:18 +0100

    remove old version of lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,56 +1,12 @@
 -- LspConfig.lua
-
--- local lls_root = os.getenv("localappdata") .. "/lua-language-server"
--- local lls_main = lls_root .. "/main.lua"
--- this could be windows specific
-
-local check_os = function()
-    local unix = vim.fn.has 'unix'
-    local windows = vim.fn.has 'windows'
-
-    if unix == 1 and windows == 0 then
-        return 'unix'
-    end
-
-    if unix == 0 and windows == 1 then
-        return 'windows'
-    end
-
-    return nil
-end
-
-local get_lua_cmd = function()
-    local my_os = check_os()
-    if my_os == 'windows' then
-        local path = os.getenv 'localappdata'
-
-        return { 'lua-language-server', '-E', path .. '/lua-language-server/main.lua' }
-    end
-    return { 'lua-language-server' }
-end
-
-local get_lexical_cmd = function()
-    local my_os = check_os()
-    if my_os == 'windows' then
-        return nil
-    end
-
-    local root_path = vim.fn.stdpath("config")
-    root_path = root_path:gsub("%nvim", "")
-
-    return {
-        root_path .. 'lexical/_build/dev/package/lexical/bin/start_lexical.sh'
-    }
-end
-
-local lua_cmd = get_lua_cmd()
-
 return {
-    -- LSP Plugins
     {
         -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
         -- used for completion, annotations and signatures of Neovim apis
         'folke/lazydev.nvim',
+        dependencies = {
+            { 'Bilal2453/luvit-meta', lazy = true }
+        },
         ft = 'lua',
         opts = {
             library = {
@@ -60,146 +16,14 @@ return {
         },
     },
 
-    { 'Bilal2453/luvit-meta', lazy = true },
-
     {
-        -- LSP Configuration & Plugins
-        'neovim/nvim-lspconfig',
-
+        "mason-org/mason.nvim",
+        opts = {},
         dependencies = {
-            -- Automatically install LSPs and related tools to stdpath for Neovim
-            { 'mason-org/mason.nvim', config = true },
-            'mason-org/mason-lspconfig.nvim',
-            'WhoIsSethDaniel/mason-tool-installer.nvim',
-            {
-                -- Notification Configuration
-                "j-hui/fidget.nvim",
-                opts = {}
-            },
-            'saghen/blink.cmp',
+            "mason-org/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
+            -- Notification Configuration
+            { "j-hui/fidget.nvim", opts = {} },
         },
-
-        opts = {
-
-            servers = {
-
-                -- Python LSP
-                ruff = {},
-
-                -- R LSP
-                r_language_server = {
-                    cmd = { 'R', '--no-echo', '-e', 'languageserver::run()' },
-                    filetypes = { 'r', 'rmd', 'Rmd' },
-                },
-
-                -- BASH LSP
-                bashls = {
-                    cmd = { 'bash-language-server', 'start' },
-                    filetypes = { 'sh' },
-                    settings = {
-                        bashIde = {
-                            globPattern = '*@(.sh|.inc|.bash|.command)',
-                        },
-                    },
-                },
-
-                marksman = {
-                    cmd = { 'marksman', 'server' },
-                    filetypes = { 'markdown', 'markdown.mdx', 'quarto' },
-                },
-
-
-                -- Lua LSP
-                lua_ls = {
-                    cmd = lua_cmd,
-                    filetypes = { 'lua' },
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = 'Replace',
-                            },
-                            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                            -- diagnostics = { disable = { 'missing-fields' } },
-                        },
-                    },
-                },
-
-                lexical = {
-                    cmd = get_lexical_cmd(),
-                    root_dir = function(fname)
-                        local lspconfig = require 'lspconfig'
-                        return lspconfig.util.root_pattern('mix.exs', '.git')(fname) or vim.loop.cwd()
-                    end,
-                    filetypes = { 'elixir', 'eelixir', 'heex' },
-                    -- optional settings
-                    settings = {},
-                },
-
-                tailwindcss = {},
-
-                html = { filetypes = { 'html', 'heex' } },
-            },
-        },
-
-        config = function(_, opts)
-            local lspconfig = require 'lspconfig'
-            local ensure_installed = vim.tbl_keys(opts.servers or {})
-
-            -- extend the list to include lua formatter
-            -- vim.list_extend(ensure_installed, { 'stylua' })
-
-            -- use mason to ensure every server is installed.
-            require('mason').setup()
-            require('mason-lspconfig').setup { ensure_installed = ensure_installed }
-            require('lspconfig.ui.windows').default_options.border = 'rounded'
-
-            -- for every server, set it up and ensure it knows about blink.
-            for server, config in pairs(opts.servers) do
-                -- passing config.capabilities to blink.cmp merges with the capabilities in your
-                -- `opts[server].capabilities, if you've defined it
-                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
-            end
-
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-                callback = function(event)
-                    local map = function(keys, func, desc)
-                        vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-                    end
-
-                    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-                    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-                    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-                    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-                    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-                    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-                    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-                    -- The following code creates a keymap to toggle inlay hints in your
-                    -- code, if the language server you are  using supports them
-                    --
-                    -- This may be unwanted, since they displace some of your code
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-                        map('<leader>th', function()
-                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-                        end, '[T]oggle Inlay [H]ints')
-                    end
-                end,
-            })
-        end,
     },
 }
