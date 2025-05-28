0fb30c38737df83c18b1527d970a3ed87204d2ab
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-28 22:06:00 +0100

    move diagnostics config to the new plugin

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -15,27 +15,27 @@ end
 vim.lsp.enable(lsp_files)
 
 
--- Configure the diagnostics
-vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-        focusable = true,
-        style = "minimal",
-        border = "rounded",
-        source = true,
-        header = "",
-        prefix = "",
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.HINT] = " ",
-            [vim.diagnostic.severity.INFO] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.ERROR] = " ",
-        },
-    },
-})
+-- -- Configure the diagnostics
+-- vim.diagnostic.config({
+--     virtual_text = false,
+--     virtual_lines = true,
+--     update_in_insert = true,
+--     underline = true,
+--     severity_sort = true,
+--     float = {
+--         focusable = true,
+--         style = "minimal",
+--         border = "rounded",
+--         source = true,
+--         header = "",
+--         prefix = "",
+--     },
+--     signs = {
+--         text = {
+--             [vim.diagnostic.severity.HINT] = "●",
+--             [vim.diagnostic.severity.INFO] = "●",
+--             [vim.diagnostic.severity.WARN] = "●",
+--             [vim.diagnostic.severity.ERROR] = "●",
+--         },
+--     },
+-- })
