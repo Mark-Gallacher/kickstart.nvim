8f58f09cb191b35a25f731c5a66cbeaca0e024f3
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2025-05-29 22:31:50 +0100

    add keymaps for windows, ensure all lsp related keymaps have the same start

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,23 +5,23 @@ local set = vim.keymap.set
 
 -- Function to make new terminal more easily
 local function new_terminal(lang)
-    vim.cmd('vsplit term://' .. lang)
+  vim.cmd('vsplit term://' .. lang)
 end
 
 local function new_terminal_python()
-    new_terminal 'python'
+  new_terminal 'python'
 end
 
 local function new_terminal_r()
-    new_terminal 'R --no-save'
+  new_terminal 'R --no-save'
 end
 
 local function new_terminal_ipython()
-    new_terminal 'ipython --no-confirm-exit'
+  new_terminal 'ipython --no-confirm-exit'
 end
 
 local function new_terminal_shell()
-    new_terminal '$SHELL'
+  new_terminal '$SHELL'
 end
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
@@ -35,10 +35,12 @@ set('n', '<leader>;q', '<cmd>q<CR>', { desc = 'Exit File' })
 set('n', '<leader>;Q', '<cmd>qa<CR>', { desc = 'Exit File' })
 
 -- INFO: DIAGNOSTIC KEYMAPS
-set('n', '[d', function() vim.diagnostic.jump({ count = 1, float = true }) end,
-    { desc = 'Go to previous [D]iagnostic message' })
-set('n', ']d', function() vim.diagnostic.jump({ count = -1, float = true }) end,
-    { desc = 'Go to next [D]iagnostic message' })
+set('n', '[d', function()
+  vim.diagnostic.jump { count = 1, float = true }
+end, { desc = 'Go to previous [D]iagnostic message' })
+set('n', ']d', function()
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = 'Go to next [D]iagnostic message' })
 
 set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 
@@ -63,8 +65,15 @@ set('t', '<C-l>', '<C-\\><C-N><C-w><C-l>', { desc = 'Move focus to the right win
 set('t', '<C-j>', '<C-\\><C-N><C-w><C-j>', { desc = 'Move focus to the lower window' })
 set('t', '<C-k>', '<C-\\><C-N><C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-set("n", "<leader>wv", "<C-w>v", { desc = "Create [W]indow [V]ertical Split" })
-set("n", "<leader>ws", "<C-w>s", { desc = "Create [W]indow [S]plit" })
+set('n', '<leader>wv', '<C-w>v', { desc = 'Create [W]indow [V]ertical Split' })
+set('n', '<leader>ws', '<C-w>s', { desc = 'Create [W]indow [S]plit' })
+set('n', '<leader>we', '<C-w>=', { desc = '[W]indow [E]qualise' })
+set('n', '<leader>wf', '<C-w>|<C-w>_', { desc = '[W]indow [F]ullscreen' })
+set('n', '<leader>wt', '<C-w>t', { desc = 'Move [W]indow to [T]ab' })
+set('n', '<leader>wl', '<C-w>L', { desc = 'Move [W]indow Right' })
+set('n', '<leader>wh', '<C-w>H', { desc = 'Move [W]indow Left' })
+set('n', '<leader>wj', '<C-w>J', { desc = 'Move [W]indow Down' })
+set('n', '<leader>wk', '<C-w>K', { desc = 'Move [W]indow Up' })
 
 -- Normally these are not good mappings, but worth trying to
 -- make navigating tabs easier this way.
@@ -77,9 +86,6 @@ set('n', '<M-.>', '<c-w>5>', { desc = 'Increase the width of the window' })
 set('n', '<M-t>', '<c-w>+', { desc = 'Increase the Height of the window -- Taller' })
 set('n', '<M-s>', '<c-w>-', { desc = 'Decrease the Height of the window -- Shorter' })
 
-set('n', '<leader>bn', vim.cmd.bnext, { desc = 'Move to Next Buffer' })
-set('n', '<leader>bp', vim.cmd.bprevious, { desc = 'Move to Previous Buffer' })
-
 -- move cursor to bottom or top of window, then centre of cursor.
 set('n', '<c-d>', '<c-d>zz', { desc = 'Move Cursor to bottom of window - then centre' })
 set('n', '<c-u>', '<c-u>zz', { desc = 'Move Cursor to top of window - then centre' })
@@ -91,35 +97,35 @@ set('v', 'K', ":m '<-2<CR>gv=gv")
 -- When you yank text and want to paste over text to replace it -
 -- this keeps the yanked text in the register, puts deleted text in _ register
 set({ 'n', 'v' }, '<leader>p', [["_dP]])
--- set({ 'n', 'v' }, '<leader>d', [["_d]])
+set({ 'n', 'v' }, '<leader>d', [["_d]])
 
 -- New terminals
 set('n', '<leader>tr', function()
-    new_terminal_r()
+  new_terminal_r()
 end, { desc = 'Create new [T]erminal - [R]' })
 
 set('n', '<leader>tp', function()
-    new_terminal_python()
+  new_terminal_python()
 end, { desc = 'Create new [T]erminal - [Python]' })
 
 set('n', '<leader>ti', function()
-    new_terminal_ipython()
+  new_terminal_ipython()
 end, { desc = 'Create new [T]erminal - [IPython]' })
 
 set('n', '<leader>ts', function()
-    new_terminal_shell()
+  new_terminal_shell()
 end, { desc = 'Create new [T]erminal - [Shell]' })
 
-
 -- Toggle virtual lines and text
-set('n', '<leader>dt', function()
-    vim.diagnostic.config {
-        virtual_lines = not vim.diagnostic.config().virtual_lines,
-        -- virtual_text = not vim.diagnostic.config().virtual_text,
-    }
-    require("tiny-inline-diagnostic").toggle()
-end, { desc = '[D]iagnostics [T]oggle - virtual lines and virtual text' })
+-- All LSP keymas start with leader g
+set('n', '<leader>gt', function()
+  vim.diagnostic.config {
+    virtual_lines = not vim.diagnostic.config().virtual_lines,
+    -- virtual_text = not vim.diagnostic.config().virtual_text,
+  }
+  require('tiny-inline-diagnostic').toggle()
+end, { desc = 'Diagnostics [T]oggle - virtual lines and virtual text' })
 
-set('n', '<leader>dh', function()
-    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { desc = '[D]iagnostics [H]ide - all' })
+set('n', '<leader>gh', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = 'Diagnostics [H]ide - all' })
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,41 +1,14 @@
 -- Read the LSP configuration Files
 local lsp_files = {}
-local lsp_dir = vim.fn.stdpath("config") .. "/lsp/"
+local lsp_dir = vim.fn.stdpath 'config' .. '/lsp/'
 
 -- Iterate over all files inside the lsp directory
-for _, file in ipairs(vim.fn.globpath(lsp_dir, "*.lua", false, true)) do
-    -- `:t` gets filename, `:r` removes extension
-    local name = vim.fn.fnamemodify(file, ":t:r")
+for _, file in ipairs(vim.fn.globpath(lsp_dir, '*.lua', false, true)) do
+  -- `:t` gets filename, `:r` removes extension
+  local name = vim.fn.fnamemodify(file, ':t:r')
 
-    table.insert(lsp_files, name)
+  table.insert(lsp_files, name)
 end
-
 
 -- Enable the LSPs
 vim.lsp.enable(lsp_files)
-
-
--- -- Configure the diagnostics
--- vim.diagnostic.config({
---     virtual_text = false,
---     virtual_lines = true,
---     update_in_insert = true,
---     underline = true,
---     severity_sort = true,
---     float = {
---         focusable = true,
---         style = "minimal",
---         border = "rounded",
---         source = true,
---         header = "",
---         prefix = "",
---     },
---     signs = {
---         text = {
---             [vim.diagnostic.severity.HINT] = "●",
---             [vim.diagnostic.severity.INFO] = "●",
---             [vim.diagnostic.severity.WARN] = "●",
---             [vim.diagnostic.severity.ERROR] = "●",
---         },
---     },
--- })
