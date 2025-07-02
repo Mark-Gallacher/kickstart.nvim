a200c532741ce9198341432d856d7703e70a8698
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2025-07-02 16:55:00 +0100

    add a new r lsp to try out - ensure languageserver isn't distracted by renv files

--- lsp/air.lua
+++ lsp/air.lua
@@ -1,7 +1,7 @@
--- R
+-- R - air lsp and formaatter
 ---@type vim.lsp.Config
 return {
-  cmd = { 'R', '--no-echo', '--no-save', '--no-init-file', '-e', 'languageserver::run()' },
+  cmd = { 'air', 'language-server' },
   filetypes = { 'r', 'rmd', 'Rmd' },
   root_dir = function(bufnr, on_dir)
     on_dir(vim.fs.root(bufnr, '.git') or vim.uv.os_homedir())
